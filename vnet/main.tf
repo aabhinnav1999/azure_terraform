43f7350615974a91d3548417f2e9f831d53a7eab
aabhinnav1999 <vdabhinavkotagiri@gmail.com>
2026-01-15 22:24:30 +0000

    updated vnet

--- vnet/main.tf
+++ vnet/main.tf
@@ -24,15 +24,29 @@ resource "azurerm_virtual_network" "example" {
     location            = azurerm_resource_group.example.location
     resource_group_name = azurerm_resource_group.example.name   
 
-    # Create subnets
+    # Create subnets here or use separate terraform resources
     
-    subnet {
-        name           = "subnet1"
-        address_prefixes = ["10.0.1.0/24"]
-    }
+    # subnet {
+    #     name           = "subnet1"
+    #     address_prefixes = ["10.0.1.0/24"]
+    # }
 
-    subnet {
-        name           = "subnet2"
-        address_prefixes = ["10.0.2.0/24"]
-    }
+    # subnet {
+    #     name           = "subnet2"
+    #     address_prefixes = ["10.0.2.0/24"]
+    # }
+}
+
+resource "azurerm_subnet" "subnet1" {
+    name                 = "subnet1"
+    resource_group_name  = azurerm_resource_group.example.name
+    virtual_network_name = azurerm_virtual_network.example.name
+    address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "subnet2" {
+    name                 = "subnet2"
+    resource_group_name  = azurerm_resource_group.example.name
+    virtual_network_name = azurerm_virtual_network.example.name
+    address_prefixes     = ["10.0.2.0/24"]
 }
